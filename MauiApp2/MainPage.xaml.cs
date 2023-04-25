b60d62e23763fd2aca5aaf720978c415d27be249
Bean <beansobre@gmail.com>
2023-04-25 14:26:15 +0800

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+
+    IDispatcherTimer timer;
+    TimeSpan elapsed = TimeSpan.Zero;
+    private bool timerStarted = false;
+    public MainPage()
+	{
+		InitializeComponent();
+
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        if (timerStarted == false)
+        {
+            timer = Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+
+            timer.Tick += (s, e) =>
+            {
+                elapsed += timer.Interval;
+                timerLabel.Text = elapsed.ToString("hh\\:mm\\:ss\\:ff");
+            };
+            CounterBtn.Text = "Pause";
+            timer.Start();
+            timerStarted = true;
+        }
+        else
+        {
+            CounterBtn.Text = "Start";
+            timer.Stop();
+            timerStarted = false;
+        }
+        
+
+    }
+
+    private void OnResetClicked(object sender, EventArgs e)
+    {
+            timer.Stop();
+            elapsed = TimeSpan.Zero;
+            CounterBtn.Text = "Start";
+    }
+
+}
+
